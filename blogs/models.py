4efd60c60626bf387bd4353b963b35aeb236ca0c
Victoria González <victoriagc4@gmail.com>
2024-05-11 11:07:05 -0400

    model added

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE, 
+    )
+
+    def __str__(self) -> str:
+        return self.title
