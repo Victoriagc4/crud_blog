27e9aba0ca1fa1926e65466ea61937692d0a999f
Victoria González <victoriagc4@gmail.com>
2024-05-18 12:17:10 -0400

    added detail view

--- blogs/models.py
+++ blogs/models.py
@@ -16,5 +16,4 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("home")
-    #reverse("post_detail", args=[str(self.id)])
+        return reverse("post_detail", args=[str(self.id)])
