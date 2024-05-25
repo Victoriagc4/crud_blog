fafbbf7280f06c3fbc6c1fd7bdd46c06de1d4f92
Victoria González <victoriagc4@gmail.com>
2024-05-25 12:21:21 -0400

    login added

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blogs.urls"))
+    path("", include("blogs.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
